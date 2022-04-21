5b5e3d03b15e8a62f35ed99fd74d3320a94931f7
LJosueCM <ljosuecm@gmail.com>
2022-04-21 00:25:45 -0500

    6. Endpoint que elimina explorers

--- app.js
+++ app.js
@@ -31,9 +31,16 @@ app.post('/v1/explorers', (req, res) => {
 
 app.put('/v1/explorers/:id', (req, res) => {
     console.log(`Api Explorers PUT request ${new Date()}`);
-    console.log(`Getting explorer with id ${req.params.id}`);
+    console.log(`Update explorer with id ${req.params.id}`);
     const requestBody = req.body
     res.status(200).json({message: "Updated!"})
+})
+
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`);
+    console.log(`Delete explorer with id ${req.params.id}`);
+    const requestBody = req.body
+    res.status(200).json({message: "Deleted"})
 })
 
 // Con esto inicializamos esta app
